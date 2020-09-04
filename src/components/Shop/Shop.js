3f94a3ae5a289b986716fbb75867da42550e5f54
Abdullah0095 <abdullah.web1995@gmail.com>
2020-09-04 18:29:31 +0600

    nan done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -12,10 +12,24 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
 
     const handleAddProduct = (product) =>{
-        const newCart = [...cart, product];
+        const toBeAddeedKey = product.key;
+        const sameProduct = cart.find(pd => pd.key === product.key);
+        let count = 1;
+        let newCart;
+        if(sameProduct){
+            const count = sameProduct.quantity + 1;
+            sameProduct.quantity = count;
+            const others = cart.filter(pd => pd.key !== toBeAddeedKey)
+            newCart = [...others, sameProduct];
+        }
+        else{
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        
+        
         setCart(newCart);
-        const sameProduct = newCart.filter(pd => pd.key === product.key);
-        const count = sameProduct.length;
+        
         addToDatabaseCart(product.key, count);
 }
     
